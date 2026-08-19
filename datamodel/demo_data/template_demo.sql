02dabf3fe512f19ce1738b9cd7de6b7f4da9a4eb
Arnaud Poncet-Montanges <ponceta@users.noreply.github.com>
2026-08-19 10:47:42 +0200

    add template demo data with no data
    add template demo data with no data

---
+++ datamodel/demo_data/template_demo.sql
@@ -0,0 +1,20 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.9
+-- Dumped by pg_dump version 16.9
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: organisation; Type: TABLE DATA; Schema: temp_od; Owner: -
+--
